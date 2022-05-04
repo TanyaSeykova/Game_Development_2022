e9b602f7d014d981c5496561a798abf2ede58223
TanyaSeykova <tanya.seykova@gmail.com>
2022-05-04 17:53:18 +0100

    Add files via upload

---
+++ Homework 3/Assets/Scripts/animateMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("horizontalRight", Input.GetAxis("Horizontal"));
+        anim.SetFloat("horizontalLeft", Input.GetAxis("Horizontal"));
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+    }
+}
